0f89286375b395f0fdd7014833f861afb8746def
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-08-09 08:09:11 +0200

    Added the logic for the math operators.

--- app/src/main/java/com/ahmedmamdouh/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/ahmedmamdouh/calculatorapplication/MainActivity.kt
@@ -7,15 +7,25 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.cachedValue
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.firstNumber
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.operation
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.operationCode
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.resultsTextView
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.secondNumber
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.setResultsText
 import kotlinx.android.synthetic.main.activity_main.*
 
+
 class MainActivity : AppCompatActivity() {
-    lateinit var resultTextView : TextView
+    lateinit var resultTextView: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        resultTextView = resultText
+
+        // A global textview object which controls the results that display on the screen
+        resultsTextView = resultText
 
         // Setting click listeners for digit buttons
         oneBtn.setOnClickListener(NumberClickedImplementer)
@@ -43,38 +53,53 @@ class MainActivity : AppCompatActivity() {
         ceBtn.setOnClickListener(ClearClickedImplementer)
         removeBtn.setOnClickListener(ClearClickedImplementer)
 
-        // Sending the results textview to the classes that handle the actions
-        NumberClickedImplementer.outputTextView = resultTextView
-        OperatorClickedImplementer.outputTextView = resultTextView
-        ClearClickedImplementer.outputTextView = resultTextView
-
-
-
     }
 
-    // Handling the event that occurs when a digit button is clicked
-    object NumberClickedImplementer : View.OnClickListener{
-        lateinit var outputTextView: TextView
+    // A class which has utility methods to control the results displayed
+    object ResultsController {
 
-        fun setTextView(textView: TextView){
-            outputTextView = textView
+        lateinit var resultsTextView: TextView
+        var firstNumber: String? = null
+        var secondNumber: String? = null
+        var operation: Boolean = false
+        var cachedValue: Int = 0
+
+        // This variable identifies the operation used. 1 for +, 2 for -, 3 for times and 4 for divided by.
+        var operationCode: Int = -1
+
+        fun setResultsText(results: String) {
+
         }
+
+        fun appendToResultsText(char: String) {
+
+        }
+    }
+
+
+    // Handling the event that occurs when a digit button is clicked
+    object NumberClickedImplementer : View.OnClickListener {
 
         /**
          * This method is responsible for responding to click events by digit buttons and the decimal point.
          * It makes sure the numbers are displayed correctly.
-         * @param v : this the button view which was clicked.
+         * @param v : this is the button view which was clicked.
          *
          */
         override fun onClick(v: View?) {
             val buttonView = v as Button
-            when(buttonView.text){
-                "0"-> if (!(outputTextView.text.equals("0")))  outputTextView.text = "${outputTextView.text}0"
-                "." -> if (!(outputTextView.text.contains(".")))  outputTextView.text = "${outputTextView.text}."
-                else -> if (!(outputTextView.text.equals("0"))) outputTextView.text =
-                    "${outputTextView.text}${buttonView.text}"
-                    else
-                    outputTextView.text = "${buttonView.text}"
+            when (buttonView.text) {
+                "0" -> if (!(resultsTextView.text.equals("0"))) resultsTextView.text =
+                    "${resultsTextView.text}0"
+                "." -> if (!(resultsTextView.text.contains("."))) resultsTextView.text =
+                    "${resultsTextView.text}."
+                else -> if (!(resultsTextView.text.equals("0")) and !operation)
+                    resultsTextView.text = "${resultsTextView.text}${buttonView.text}"
+                else {
+
+                    resultsTextView.text = "${buttonView.text}"
+                    operation = false
+                }
 
             }
 
@@ -84,30 +109,105 @@ class MainActivity : AppCompatActivity() {
 
 
     // Handling the event that occurs when an operator button is clicked
-    object OperatorClickedImplementer : View.OnClickListener{
-        lateinit var outputTextView: TextView
+    object OperatorClickedImplementer : View.OnClickListener {
 
-        fun setTextView(textView: TextView){
-            outputTextView = textView
-        }
-
+        /**
+         * This method is responsible for responding to click events by operator buttons
+         * It makes sure the operations are calculated and displayed correctly.
+         * @param v : this is the button view which was clicked.
+         *
+         */
         override fun onClick(v: View?) {
+            val buttonView = v as Button
+            when (buttonView.text) {
+                "+" -> {
+                    if (firstNumber == null) {
+                        firstNumber = resultsTextView.text.toString()
+                        operation = true
+
+                    } else if (!operation) {
+                        updateResults()
+                        operation = true
+                    }
+                    operationCode = 1
+
+                }
+
+                "-" -> {
+                    if (firstNumber == null) {
+                        firstNumber = resultsTextView.text.toString()
+                        operation = true
+                    } else if (!operation) {
+                        updateResults()
+                        operation = true
+                    }
+                    operationCode = 2
+                }
+
+                "x" -> {
+                    if (firstNumber == null) {
+                        firstNumber = resultsTextView.text.toString()
+                        operation = true
+                    } else if (!operation) {
+                        updateResults()
+                        operation = true
+                    }
+                    operationCode = 3
+                }
+
+                "/" -> {
+                    if (firstNumber == null) {
+                        firstNumber = resultsTextView.text.toString()
+                        operation = true
+                    } else if (!operation) {
+                        updateResults()
+                        operation = true
+                    }
+                    operationCode = 4
+                }
+
+                "=" -> {
+                    if(firstNumber == null) {
+
+                    }
+                    else {
+                        updateResults()
+                        firstNumber = null
+                    }
+                }
+            }
+
+
 
         }
 
+        /**
+         * This method is responsible for updating the results when different operations occur
+         */
+        private fun updateResults() {
+            secondNumber = resultsTextView.text.toString()
+            cachedValue = secondNumber!!.toInt()
+            when (operationCode) {
+                1 -> cachedValue = firstNumber!!.toInt() + secondNumber!!.toInt()
+                2 -> cachedValue = firstNumber!!.toInt() - secondNumber!!.toInt()
+                3 -> cachedValue = firstNumber!!.toInt() * secondNumber!!.toInt()
+                4 -> cachedValue = firstNumber!!.toInt() / secondNumber!!.toInt()
+            }
+            resultsTextView.text = "${cachedValue}"
+            firstNumber = "${cachedValue}"
+            secondNumber = null
+        }
     }
 
-    // Handling the event that occurs when a clear button is clicked
-    object ClearClickedImplementer : View.OnClickListener{
-        lateinit var outputTextView: TextView
 
-        fun setTextView(textView: TextView){
-            outputTextView = textView
+
+        // Handling the event that occurs when a clear button is clicked
+        object ClearClickedImplementer : View.OnClickListener {
+
+
+            override fun onClick(v: View?) {
+
+            }
+
         }
-
-        override fun onClick(v: View?) {
-
-        }
-
     }
-}

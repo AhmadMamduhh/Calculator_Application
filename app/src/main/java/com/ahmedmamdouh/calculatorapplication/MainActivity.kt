31be2de314ee2eb2917b515307f20c32bf33dfd6
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-08-10 10:00:37 +0200

    Added division by zero error fix, fixed some bugs in the layout and added logic for the positive/negative button.

--- app/src/main/java/com/ahmedmamdouh/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/ahmedmamdouh/calculatorapplication/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.ahmedmamdouh.calculatorapplication
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.MAX_NUMBER_SIZE
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.MAX_RUNNING_OPERATIONS
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.cachedValue
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.context
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.divisionByZeroFlag
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.firstNumber
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.historyTextView
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.operation
@@ -42,13 +48,16 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main_dark)
+        setContentView(R.layout.activity_main)
 
         // A global TextView object which controls the results that display on the screen
         resultsTextView = resultText
 
         // A global TextView object which controls the history of the calculations
         historyTextView = historyText
+
+        // Passing the context to the ResultsController class
+        context = this
 
 
         // Setting click listeners for digit buttons
@@ -84,10 +93,16 @@ class MainActivity : AppCompatActivity() {
 
         lateinit var resultsTextView: TextView
         lateinit var historyTextView: TextView
+        lateinit var context: Context
         var firstNumber: String? = null
         var secondNumber: String? = null
         var operation: Boolean = false
         var cachedValue: Int = 0
+        var divisionByZeroFlag = false
+
+        // Important constants
+        const val MAX_RUNNING_OPERATIONS = 40
+        const val MAX_NUMBER_SIZE = 10
 
         // This variable identifies the operation used. 1 for +, 2 for -, 3 for times and 4 for divided by.
         var operationCode: Int = -1
@@ -112,11 +127,38 @@ class MainActivity : AppCompatActivity() {
          *
          */
         override fun onClick(v: View?) {
+
+            // Checking if a division by zero has occurred
+            if(divisionByZeroFlag){
+                resultsTextView.text = "0"
+                resultsTextView.textSize = 60f
+                historyTextView.text = ""
+                divisionByZeroFlag = false
+                operation = true
+            }
+
+            // Checking if maximum no. of possible running operations has been achieved
+            if (historyTextView.text.toString().length >= MAX_RUNNING_OPERATIONS) {
+                Toast.makeText(context, R.string.maximum_error_1, Toast.LENGTH_LONG).show()
+                return
+            }
+
+            // Checking if maximum no. of digits in a number has been achieved
+            if ((resultsTextView.text.toString().length >= MAX_NUMBER_SIZE) and !operation) {
+                Toast.makeText(context, R.string.maximum_error_2, Toast.LENGTH_LONG).show()
+                return
+            }
+
+
             val buttonView = v as Button
             when (buttonView.text) {
-                "0" -> if (!(resultsTextView.text.equals("0"))) resultsTextView.text =
+                "0" -> if (!(resultsTextView.text.equals("0")) and !operation) resultsTextView.text =
                     "${resultsTextView.text}0"
-                "." -> if (!(resultsTextView.text.contains("."))) resultsTextView.text =
+                else {
+                    resultsTextView.text = "${buttonView.text}"
+                    operation = false
+                }
+                "." -> if (!(resultsTextView.text.contains(".")) and !operation) resultsTextView.text =
                     "${resultsTextView.text}."
                 else -> if (!(resultsTextView.text.equals("0")) and !operation)
                     resultsTextView.text = "${resultsTextView.text}${buttonView.text}"
@@ -146,6 +188,8 @@ class MainActivity : AppCompatActivity() {
             val buttonView = v as Button
             when (buttonView.text) {
                 "+" -> {
+                    if(divisionByZeroFlag)
+                        return
                     if (firstNumber == null) {
                         firstNumber = resultsTextView.text.toString()
                         historyTextView.text = "$firstNumber + "
@@ -161,6 +205,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 "-" -> {
+                    if(divisionByZeroFlag)
+                        return
+
                     if (firstNumber == null) {
                         firstNumber = resultsTextView.text.toString()
                         historyTextView.text = "$firstNumber - "
@@ -174,6 +221,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 "x" -> {
+                    if(divisionByZeroFlag)
+                        return
+
                     if (firstNumber == null) {
                         firstNumber = resultsTextView.text.toString()
                         historyTextView.text = "$firstNumber x "
@@ -187,6 +237,9 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 "/" -> {
+                    if(divisionByZeroFlag)
+                        return
+
                     if (firstNumber == null) {
                         firstNumber = resultsTextView.text.toString()
                         historyTextView.text = "$firstNumber / "
@@ -200,6 +253,18 @@ class MainActivity : AppCompatActivity() {
                 }
 
                 "=" -> {
+                    if(divisionByZeroFlag) {
+                        operation = false
+                        firstNumber = null
+                        secondNumber = null
+                        cachedValue = 0
+                        operationCode = -1
+                        resultsTextView.text = "0"
+                        historyTextView.text = ""
+                        resultsTextView.textSize = 60f
+                        return
+                    }
+
                     if (firstNumber == null) {
                         historyTextView.text = "${resultsTextView.text} = "
                     } else {
@@ -207,6 +272,19 @@ class MainActivity : AppCompatActivity() {
                         updateResults()
                         firstNumber = null
                     }
+                }
+
+                "+/-" -> {
+                    if(divisionByZeroFlag)
+                        return
+
+                    if (resultsTextView.text.toString() == "0")
+                        return
+                    if (resultsTextView.text.toString()[0] == '-')
+                        resultsTextView.text = resultsTextView.text.toString()
+                            .substring(1, resultsTextView.text.toString().length)
+                    else
+                        resultsTextView.text = "-" + resultsTextView.text.toString()
                 }
             }
 
@@ -223,7 +301,17 @@ class MainActivity : AppCompatActivity() {
                 1 -> cachedValue = firstNumber!!.toInt() + secondNumber!!.toInt()
                 2 -> cachedValue = firstNumber!!.toInt() - secondNumber!!.toInt()
                 3 -> cachedValue = firstNumber!!.toInt() * secondNumber!!.toInt()
-                4 -> cachedValue = firstNumber!!.toInt() / secondNumber!!.toInt()
+                4 -> {
+                    if(secondNumber!!.toInt() == 0) {
+                        firstNumber = null
+                        secondNumber = null
+                        resultsTextView.text = context.getString(R.string.division_by_zero_error)
+                        resultsTextView.textSize = 30f
+                        divisionByZeroFlag = true
+                        return
+                    }
+
+                    cachedValue = firstNumber!!.toInt() / secondNumber!!.toInt()}
             }
             resultsTextView.text = "${cachedValue}"
             firstNumber = "${cachedValue}"
@@ -243,13 +331,16 @@ class MainActivity : AppCompatActivity() {
          */
         override fun onClick(v: View?) {
             when (v!!.id) {
-                R.id.removeBtn ->
+                R.id.removeBtn -> {
+                    if (divisionByZeroFlag) return
+
                     if ((resultsTextView.text.toString().length == 1) and !operation)
                         resultsTextView.text = "0"
                     else if (!(resultsTextView.text.equals("0")) and !operation) {
                         resultsTextView.text = resultsTextView.text.toString()
                             .substring(0, resultsTextView.text.toString().length - 1)
                     }
+                }
 
                 R.id.cBtn -> {
                     operation = false
@@ -259,11 +350,14 @@ class MainActivity : AppCompatActivity() {
                     operationCode = -1
                     resultsTextView.text = "0"
                     historyTextView.text = ""
+                    if(divisionByZeroFlag) resultsTextView.textSize = 60f
                 }
 
                 R.id.ceBtn -> {
                     resultsTextView.text = "0"
                     if (operation) operation = false
+                    if(divisionByZeroFlag) resultsTextView.textSize = 60f
+
                 }
             }
 

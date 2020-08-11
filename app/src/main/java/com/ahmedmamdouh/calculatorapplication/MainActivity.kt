0f99e4af24cdc943667fdfe515ce2100e9d5121a
Ahmed Mamdouh <mamdu7x@gmail.com>
2020-08-11 02:22:51 +0200

    Added formatBigNumbers method which handles the presentation of big numbers on the screen.

--- app/src/main/java/com/ahmedmamdouh/calculatorapplication/MainActivity.kt
+++ app/src/main/java/com/ahmedmamdouh/calculatorapplication/MainActivity.kt
@@ -13,6 +13,7 @@ import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.cac
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.context
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.divisionByZeroFlag
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.firstNumber
+import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.formatBigNumbers
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.historyTextView
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.operation
 import com.ahmedmamdouh.calculatorapplication.MainActivity.ResultsController.operationCode
@@ -103,15 +104,21 @@ class MainActivity : AppCompatActivity() {
         lateinit var scrollView: HorizontalScrollView
         var firstNumber: String? = null
         var secondNumber: String? = null
+
+        // This boolean is a flag which if true indicates that there's a math operation occuring
         var operation: Boolean = false
+
+        // This is a variable which acts as an accumulator in running operations
         var cachedValue: Double = 0.0
+
+        // This boolean is a flag which if true indicates that a division by zero error has happened
         var divisionByZeroFlag = false
 
         // Important constants
         const val MAX_RUNNING_OPERATIONS = 100
-        const val MAX_NUMBER_SIZE = 10
+        const val MAX_NUMBER_SIZE = 12
 
-        // This variable identifies the operation used. 1 for +, 2 for -, 3 for times and 4 for divided by.
+        // This variable identifies the operation used. 1 for +, 2 for -, 3 for x and 4 for /
         var operationCode: Int = -1
 
         /**
@@ -119,6 +126,46 @@ class MainActivity : AppCompatActivity() {
          **/
         fun adjustTextView() {
             scrollView.post { scrollView.fullScroll(ScrollView.FOCUS_RIGHT) }
+        }
+
+        /**
+         * This method formats numbers that are too big to display
+         * @param number : The string which will be formatted
+         * @return formattedNumber : The string after being formatted to fit on the screen
+         **/
+        fun formatBigNumbers(number : String) : String {
+            var formattedString = ""
+            var count = 0
+            var eFlag = false
+            var dotFound = false
+            for(digit in number){
+                if((digit != '.') and !dotFound){
+                    formattedString += digit
+                    dotFound = true
+                }
+                else if((count < 10) and !eFlag){
+                    if(digit == 'E') {
+                        eFlag = true
+                        formattedString += digit
+                        count = 11
+                    }
+                    else {
+                        formattedString += digit
+                        count += 1
+                    }
+
+                }
+                else if(count == 10){
+                    if(digit == 'E'){
+                        formattedString += digit
+                        count += 1
+                        eFlag= true
+                    }
+                }
+                else if(eFlag and (count > 10))
+                    formattedString += digit
+            }
+            return formattedString
         }
 
         fun setResultsText(results: String) {
@@ -174,6 +221,10 @@ class MainActivity : AppCompatActivity() {
                 }
                 "." -> if (!(resultsTextView.text.contains(".")) and !operation) resultsTextView.text =
                     "${resultsTextView.text}."
+                else if(operation){
+                    resultsTextView.text = "0."
+                    operation = false
+                }
                 else -> if (!(resultsTextView.text.equals("0")) and !operation)
                     resultsTextView.text = "${resultsTextView.text}${buttonView.text}"
                 else {
@@ -336,7 +387,12 @@ class MainActivity : AppCompatActivity() {
             }
             val df = DecimalFormat("#.####")
             df.roundingMode = RoundingMode.CEILING
-            resultsTextView.text = df.format(cachedValue)
+
+            if(df.format(cachedValue).length >= 14)
+                resultsTextView.text = "${formatBigNumbers(df.format(cachedValue).toDouble().toString())}"
+            else
+                resultsTextView.text = df.format(cachedValue)
+
             firstNumber = "$cachedValue"
             secondNumber = null
         }
